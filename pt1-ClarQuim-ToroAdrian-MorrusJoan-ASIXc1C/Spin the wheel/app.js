db95263de3f518a23745628581f482d332e0abb4
JoanMorrus <123154730+JoanMorrus@users.noreply.github.com>
2023-05-08 10:20:18 +0000

    a

--- pt1-ClarQuim-ToroAdrian-MorrusJoan-ASIXc1C/Spin the wheel/app.js
+++ pt1-ClarQuim-ToroAdrian-MorrusJoan-ASIXc1C/Spin the wheel/app.js
@@ -1,25 +1,45 @@
-const noms = ['Anna', 'Maria', 'Joan', 'David', 'Laura', 'Marc'];
+const noms = [
+  "Nom 1",
+  "Nom 2",
+  "Nom 3",
+  "Nom 4",
+  "Nom 5",
+  "Nom 6",
+  "Nom 7",
+  "Nom 8",
+  "Nom 9",
+  "Nom 10"
+];
 
-const btnSeleccionarNom = document.getElementById('seleccionar-nom');
-const nomContainer = document.getElementById('nom-container');
-const nomElement = document.getElementById('nom');
-const rodamentSound = document.getElementById('so-rodament');
-const aturadaSound = document.getElementById('so-aturada');
+const nomContainer = document.getElementById("nom-container");
+const nomText = document.getElementById("nom");
 
 function seleccionarNom() {
-  const index = Math.floor(Math.random() * noms.length);
-  const nom = noms[index];
-  nomElement.textContent = nom;
-  nomContainer.classList.add('nom-animation');
-  rodamentSound.play();
-  setTimeout(() => {
-    nomContainer.classList.remove('nom-animation');
-    nomContainer.classList.add('nom-animation-stop');
-    aturadaSound.play();
-    setTimeout(() => {
-      nomContainer.classList.remove('nom-animation-stop');
-    }, 500);
-  }, 5000);
+  const randomNom = noms[Math.floor(Math.random() * noms.length)];
+  const anglePerNom = 360 / noms.length;
+  let currentAngle = 0;
+  let newAngle = 0;
+  const animationTime = 3000; // temps de 3 segons
+  
+  const animationInterval = setInterval(() => {
+    currentAngle += anglePerNom;
+    nomContainer.style.transform = `rotate(${currentAngle}deg)`;
+    
+    if (currentAngle >= 360) {
+      currentAngle = 0;
+    }
+    
+    if (currentAngle === newAngle) {
+      clearInterval(animationInterval);
+      nomText.textContent = randomNom;
+      nomContainer.classList.remove("nom-animation");
+      nomContainer.classList.add("nom-animation-stop");
+    }
+  }, animationTime / noms.length);
+  
+  nomContainer.classList.add("nom-animation");
 }
 
-btnSeleccionarNom.addEventListener('click', seleccionarNom);
+const seleccionarNomBtn = document.getElementById("seleccionar-nom");
+seleccionarNomBtn.addEventListener("click", seleccionarNom);
+
