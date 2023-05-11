d9c44c3f71db0fc64eb64f9ac8a2a7b289729364
quim-clar <123154807+quim-clar@users.noreply.github.com>
2023-05-11 14:22:34 +0000

    A

--- pt1-ClarQuim-ToroAdrian-MorrusJoan-ASIXc1C/Rellotje-Temporitzador/script.js
+++ pt1-ClarQuim-ToroAdrian-MorrusJoan-ASIXc1C/Rellotje-Temporitzador/script.js
@@ -1,60 +1,74 @@
-function stopCountdown() {
-  clearInterval(timer);
-  countdown = null;
-  startButton.disabled = false;
-  stopButton.disabled = true;
-  audio.pause();
-}
+const minutesInput = document.getElementById("minutes-input");
+const secondsInput = document.getElementById("seconds-input");
+const durationMinutesInput = document.getElementById("duration-minutes-input");
+const durationSecondsInput = document.getElementById("duration-seconds-input");
+const soundSelect = document.getElementById("sound-select");
+const startButton = document.getElementById("start-btn");
+const stopButton = document.getElementById("stop-btn");
+const timerDisplay = document.getElementById("timer");
+const sound = document.getElementById("sound");
+const alarmTimeInput = document.getElementById("alarm-time-input");
 
-function playSoundEffect() {
-  audio.src = soundEffect.value;
-  audio.currentTime = 0;
-  audio.play();
-}
 
-function updateClock() {
-  remainingTime = Math.round((endTime - Date.now()) / 1000);
-  if (remainingTime < 0) {
-    clearInterval(timer);
-    countdown = null;
-    startButton.disabled = false;
-    stopButton.disabled = true;
-    playSoundEffect();
+let countdown;
+let duration;
+let startTime;
+let elapsedTime = 0;
+
+function startTimer() {
+  let now = new Date();
+  let alarmTime = new Date(alarmTimeInput.value);
+  if (alarmTimeInput.value && alarmTime < now) {
+    alarmTime.setDate(alarmTime.getDate() + 1);
+  }
+
+  if (alarmTimeInput.value) {
+    duration = (alarmTime - now) / 1000;
+  } else if (minutesInput.value || secondsInput.value) {
+    duration = (Number(minutesInput.value) || 0) * 60 + (Number(secondsInput.value) || 0);
+  } else {
+    duration = (Number(durationMinutesInput.value) || 0) * 60 + (Number(durationSecondsInput.value) || 0);
+  }
+
+  if (isNaN(duration) || duration <= 0) {
+    alert("La duració ha de ser un número vàlid i major que zero.");
     return;
   }
-  let hours = Math.floor(remainingTime / 3600);
-  let minutes = Math.floor((remainingTime % 3600) / 60);
-  let seconds = remainingTime % 60;
-  digitalClock.textContent = `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+
+  startTime = Date.now();
+  stopButton.disabled = false;
+  startButton.disabled = true;
+
+  countdown = setInterval(() => {
+    const elapsed = Math.floor((Date.now() - startTime) / 1000);
+    const remaining = duration - elapsed;
+
+    if (remaining < 0) {
+      clearInterval(countdown);
+      timerDisplay.textContent = "00:00";
+      sound.src = soundSelect.value;
+      sound.play();
+      startButton.disabled = false;
+      stopButton.disabled = true;
+    } else {
+      const minutes = Math.floor(remaining / 60);
+      const seconds = remaining % 60;
+      timerDisplay.textContent = `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+    }
+  }, 1000);
 }
 
-function padZero(number) {
-  return number.toString().padStart(2, "0");
-}
-
-startButton.addEventListener("click", () => {
-  if (countdown === null) {
-    if (timePicker.value) {
-      let [hours, minutes] = timePicker.value.split(":");
-      remainingTime = hours * 3600 + minutes * 60;
-    } else if (countdownInput.value) {
-      let [hours, minutes, seconds] = countdownInput.value.split(":");
-      remainingTime = hours * 3600 + minutes * 60 + seconds * 1;
+    
+    function stopTimer() {
+    clearInterval(countdown);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    elapsedTime += Math.floor((Date.now() - startTime) / 1000);
+    duration = 0;
+    timerDisplay.textContent = "00:00";
+    sound.pause();
+    sound.currentTime = 0;
     }
-    if (remainingTime > 0) {
-      countdown = true;
-      startCountdown();
-    }
-  }
-});
-
-stopButton.addEventListener("click", () => {
-  if (countdown !== null) {
-    stopCountdown();
-  }
-});
-
-soundEffect.addEventListener("change", () => {
-  playSoundEffect();
-});
-
+    
+    startButton.addEventListener("click", startTimer);
+    stopButton.addEventListener("click", stopTimer);
